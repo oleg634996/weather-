160f7c442da93414752f65c78f5582f0a001f85c
oleg634996 <olegkolodiy22@gmail.com>
2022-12-22 17:16:55 +0200

    sdsad

--- src/components/API/Api.js
+++ src/components/API/Api.js
@@ -4,7 +4,7 @@ const API = "121866bca3a5499c9bd92044222405";
 
 function ForecastFetch(event) {
   const fetch = axios.get(
-    `http://api.weatherapi.com/v1/forecast.json?key=${API}&q=${event}&days=5`
+    `https://api.weatherapi.com/v1/forecast.json?key=${API}&q=${event}&days=5`
   );
   return fetch;
 }
